c31a54d6d7a673336b87bbb00db0386ab3da993c
shopno8860 <shopno8860@gmail.com>
2025-05-15 03:27:54 +0600

    Add project files.

---
+++ Online Quiz App/Migrations/20250422161035_hibi.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Online_Quiz_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class hibi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
